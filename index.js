201def62d0a97ef5074979aee89380058fe03c52
kyyyujin <mmokka3100@gmail.com>
2026-05-05 02:34:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -408,4 +408,4 @@ app.get('/', (req, res) => res.send('Album Rater Bot — OK'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-    
+                                   
